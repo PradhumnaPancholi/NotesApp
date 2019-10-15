505b80eff8307806073c8c076bb1c50cd1efe9c8
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-10-15 04:51:11 -0400

    got working functionality

--- Note.py
+++ Note.py
@@ -6,12 +6,8 @@ class Note:
 
     # method to create a note
     def create_note(self):
-        # to take user input for note
-        note_title = input("Enter a title for your note : ")
-        note_body = input("Enter your data")
-
         # to create a file and write data for user note
-        note_file = open(note_title, "w")
-        note_title.write(note_body)
+        note_file = open(self.title, "w")
+        note_file.write(self.body)
 
         print("\n Congratulations, note saved")
---
+++ app.py
@@ -0,0 +1,10 @@
+from Note import Note
+
+print("Welcome, Let's start taking notes !!!")
+
+# to take user input for note
+note_title = input("Enter a title for your note : ")
+note_body = input("Enter your data")
+
+new_note = Note(note_title, note_body)
+new_note.create_note()
